33e286fb617015a7454b92aacf8cc89a3d985914
dgos <dawidgos25@gmail.com>
2023-10-03 22:28:23 +0200

    added inserting random satellites domains with new table

--- migrations/Version20231003194254.php
+++ migrations/Version20231003194254.php
@@ -6,6 +6,7 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use NumberFormatter;
 
 final class Version20231003194254 extends AbstractMigration
 {
@@ -16,8 +17,22 @@ final class Version20231003194254 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
+
         if(!$schema->hasTable('satellites')) {
-            $this->addSql('CREATE TABLE satellites (id INT AUTO_INCREMENT NOT NULL, domain VARCHAR(255) NOT NULL, recovery_link TEXT NOT NULL, flight_date_label_translation_key VARCHAR(255) NOT NULL, visa_type_key_name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+            $this->addSql('CREATE TABLE satellites (
+                id INT AUTO_INCREMENT NOT NULL, domain VARCHAR(255) NOT NULL, recovery_link TEXT NOT NULL, 
+                flight_date_label_translation_key VARCHAR(255) NOT NULL, visa_type_key_name VARCHAR(255) DEFAULT NULL, 
+                PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
+            );
+
+            $formatter = new NumberFormatter('en', NumberFormatter::SPELLOUT);
+            for($i = 1; $i < 10; $i++) {
+                $number = $formatter->format($i);
+                $this->addSql("INSERT INTO evisa_express.satellites
+                            (`domain`, recovery_link, flight_date_label_translation_key, visa_type_key_name)
+                            VALUES('domain-$number.com', 'https://{{domain}}/form-recovery?hash={{hash}}', 'flight date', NULL);"
+                );
+            }
         }
     }
 
