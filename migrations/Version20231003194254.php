b1b49180e8e48aa602bc6fd29008c9db88217c6f
dgos <dawidgos25@gmail.com>
2023-10-23 18:48:07 +0200

    formatting

--- migrations/Version20231003194254.php
+++ migrations/Version20231003194254.php
@@ -17,8 +17,7 @@ final class Version20231003194254 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-
-        if(!$schema->hasTable('satellites')) {
+        if (!$schema->hasTable('satellites')) {
             $this->addSql('CREATE TABLE satellites (
                 id INT AUTO_INCREMENT NOT NULL, domain VARCHAR(255) NOT NULL, recovery_link TEXT NOT NULL, 
                 flight_date_label_translation_key VARCHAR(255) NOT NULL, visa_type_key_name VARCHAR(255) DEFAULT NULL, 
@@ -26,7 +25,7 @@ final class Version20231003194254 extends AbstractMigration
             );
 
             $formatter = new NumberFormatter('en', NumberFormatter::SPELLOUT);
-            for($i = 1; $i < 10; $i++) {
+            for ($i = 1; $i < 10; $i++) {
                 $number = $formatter->format($i);
                 $this->addSql("INSERT INTO evisa_express.satellites
                             (`domain`, recovery_link, flight_date_label_translation_key, visa_type_key_name)
@@ -38,7 +37,7 @@ final class Version20231003194254 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        if($schema->hasTable('satellites')) {
+        if ($schema->hasTable('satellites')) {
             $this->addSql('DROP TABLE satellites');
         }
     }
