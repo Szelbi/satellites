202300abc7e80dfdb409f934634332041ae7c4e8
dgos <dawidgos25@gmail.com>
2024-10-05 13:50:21 +0200

    try catch for api service connect

--- src/Service/WeatherApiService.php
+++ src/Service/WeatherApiService.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Dto\WeatherApiResponseDto;
+use Symfony\Component\HttpClient\Exception\ClientException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 readonly class WeatherApiService
@@ -43,12 +47,10 @@ readonly class WeatherApiService
         );
     }
 
-
     /**
-     * @param string $city
-     * @return mixed
-     */
-    public function getFromCurl(string $city): mixed
+     * @throws BadRequestHttpException
+    */
+    public function getFromCurl(string $city): array
     {
         $apiKey = $this->weatherApiKey;
         $curl = curl_init();
@@ -57,31 +59,40 @@ readonly class WeatherApiService
             CURLOPT_URL => self::URL . "?key=$apiKey&q=$city&aqi=no",
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_HTTPGET => true,
         ]);
 
         $response = curl_exec($curl);
         curl_close($curl);
-        return json_decode($response, true);
+
+        $response = json_decode($response, true);
+
+        if (isset($response['error'])) {
+            throw new BadRequestHttpException('An error occurred: ' . $response['error']['message'], null, $response['error']['code']);
+        }
+
+        return $response;
     }
 
     /**
-     * @param string $city
-     * @return array
+     * @throws BadRequestHttpException
      */
     public function getFromSymfonyClient(string $city): array
     {
-        $response = $this->client->request('GET', self::URL, [
-            'query' => [
-                'key' => $this->weatherApiKey,
-                'q' => $city,
-                'aqi' => 'no',
-            ],
-        ]);
+        try {
+            $response = $this->client->request('GET', self::URL, [
+                'query' => [
+                    'key' => $this->weatherApiKey,
+                    'q' => $city,
+                    'aqi' => 'no',
+                ],
+            ]);
+            return $response->toArray();
+        } catch (ClientException $e) {
+            throw new BadRequestHttpException('An error occurred: ' . $e->getMessage(), null, $e->getCode());
+        }
 
-        return $response->toArray();
     }
 }
 
