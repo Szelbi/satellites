97c18427d2a283fa7ca5e0795fd0030145639544
dgos <dawidgos25@gmail.com>
2025-07-11 22:51:22 +0200

    Fixed logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,11 +23,5 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
-    }
-
-    #[Route('/logout', name: 'app_logout')]
-    public function logout(Security $security): void
-    {
-        $security->logout();
     }
 }
